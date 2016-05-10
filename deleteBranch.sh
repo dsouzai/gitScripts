402a598b02c60821e1fd357a56f193bd115a0968
Irwin D'Souza <dsouzai@ca.ibm.com>
2016-05-10 10:21:45 -0400

    Added a delete branch script

---
+++ deleteBranch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/sh
+
+if [ -z "$1" ]
+then
+   echo "Need to specify a branch name"
+   exit
+fi
+
+echo "Will delete branch $1 both locally and remotely"
+
+# Switch to the clean branch
+git checkout clean
+
+# Delete branch locally
+git branch -D $1
+
+# Delete branch remotely
+git push origin --delete $1
