10db36d83d819fb08facc3f239ee2ba1aafeefb6
Irwin D'Souza <dsouzai@ca.ibm.com>
2016-05-17 14:48:07 -0400

    Changed pushForMergeRequest script to save a backup instead of creating a new branch to rebase on to master

--- pushForMergeRequest.sh
+++ pushForMergeRequest.sh
@@ -19,16 +19,22 @@ else
    PUSH_BRANCH=1
 fi
 
-echo "Will create a new branch ${1}_final from $1 and rebase HEAD~$2"
+echo "Will create a new branch ${1}_backup from $1 and rebase HEAD~$2"
 
 # Sync and update the repos
 echo gitSyncRepos.sh
+
+# Delete backup if it exists
+echo deleteBranch.sh ${1}_backup
 
 # Switch to the branch
 echo git checkout $1
 
 # Checkout new branch for rebasing
-echo git checkout -b ${1}_final
+echo git checkout -b ${1}_backup
+
+# Switch back to the branch
+echo git checkout $1
 
 # Rebase onto master
 echo git rebase master
@@ -39,7 +45,8 @@ echo git rebase -i HEAD~$2
 # Push the branch
 if [ $PUSH_BRANCH == "1" ]
 then
-   echo git push origin ${1}_final
+   echo git push origin ${1}
 else
    # worth echoing the above command
+   echo --
 fi
