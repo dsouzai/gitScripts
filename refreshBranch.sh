48bd78aa19a2bbd24ba1896a29abba7e2e4a4260
Irwin D'Souza <dsouzai@ca.ibm.com>
2016-05-24 15:52:01 -0400

    Added refresh branch script

---
+++ refreshBranch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+
+if [ -z "$1" ]
+then
+   echo "Need to specify a branch name"
+   exit
+fi
+
+echo "Will rebase branch $1 onto clean"
+
+# Switch to the clean branch
+git checkout $1
+
+# Rebase onto clean
+git rebase clean
