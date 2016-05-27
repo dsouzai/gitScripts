b505fa3e811d28cc401d4b8296d7123da113e491
Irwin D'Souza <dsouzai@ca.ibm.com>
2016-05-27 10:02:00 -0400

    improved newBranch.sh

--- newBranch.sh
+++ newBranch.sh
@@ -6,13 +6,17 @@ then
    exit
 fi
 
+if [ -z "$2" ]
+then
+   commit="clean"
+else
+   commit=$2
+fi
+
 # Sanity check
 . sanityCheck.sh $1
 
-echo "Will create a new branch $1 from clean"
+echo "Will create a new branch $1 from $commit"
 
-# Switch to the clean branch
-git checkout clean
-
-# Branch off of clean branch
-git checkout -b $1
+# Create new branch
+git checkout -b $1 $commit
