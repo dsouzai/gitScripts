611602612bd164e19409bf41b1c6ba2b3bfb915d
Irwin D'Souza <dsouzai@ca.ibm.com>
2017-09-21 16:26:19 -0400

    Update sync script to be more general

--- gitSyncRepos.sh
+++ gitSyncRepos.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/sh
 
-for repo in omr openj9
+pushd . > /dev/null
+cd /home/dsouzai/dev/gitSync
+
+for repo in omr openj9 pi kca
 do
 
    cd $repo
+   echo "================================="
+   echo "Start syncing $repo"
+   echo "================================="
    syncRepos.sh
+   echo "================================="
+   echo "Finish syncing $repo"
+   echo "================================="
+
    cd ..
 
 done
+
+popd > /dev/null
