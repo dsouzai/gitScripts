358468b4395c74f6a3b182edf0612b2820064c2e
Irwin D'Souza <dsouzai.gh@gmail.com>
2022-02-16 18:01:36 -0500

    Improve gitSync script
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- gitSyncRepos.sh
+++ gitSyncRepos.sh
@@ -3,33 +3,29 @@
 pushd . > /dev/null
 cd /home/dsouzai/dev/gitSync
 
-for repo in omr openj9 kca
+repos="omr:master openj9:master openj9-openjdk-jdk11:openj9 openj9-openjdk-jdk17:openj9 kca:master pi:standalone"
+
+regex="((\w|\d|-)+):((\w|\d|-)+)"
+
+for keyval in $repos
 do
+  if [[ $keyval =~ $regex ]]
+  then 
+    repo="${BASH_REMATCH[1]}"
+    upstream="${BASH_REMATCH[3]}"
 
-   cd $repo
-   echo "================================="
-   echo "Start syncing $repo"
-   echo "================================="
-   syncRepos.sh
-   echo "================================="
-   echo "Finish syncing $repo"
-   echo "================================="
-   cd ..
-
+    cd $repo
+    echo "================================="
+    echo "Start syncing $repo"
+    echo "================================="
+    syncRepos.sh $upstream
+    echo "================================="
+    echo "Finish syncing $repo"
+    echo "================================="
+    cd ..
+  else
+    echo "Skipping $keyval"
+  fi
 done
-
-
-# PI
-repo="pi"
-cd $repo
-echo "================================="
-echo "Start syncing $repo"
-echo "================================="
-syncRepos.sh standalone
-echo "================================="
-echo "Finish syncing $repo"
-echo "================================="
-cd ..
-
 
 popd > /dev/null
