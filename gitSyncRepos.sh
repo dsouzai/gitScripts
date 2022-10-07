1a5ca089fd891f38819c89c7fbeefa535dfe5c66
Irwin D'Souza <dsouzai.gh@gmail.com>
2022-10-07 12:10:56 -0400

    More changes for WSL
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- gitSyncRepos.sh
+++ gitSyncRepos.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 pushd . > /dev/null
 cd ~/dev/gitSync
 
-repos="omr:master openj9:master openj9-openjdk-jdk11:openj9 kca:master pi:standalone"
+repos="omr:master:1 openj9:master:1 openj9-openjdk-jdk11:openj9:0 kca:master:1 pi:standalone:1"
 
-regex="((\w|\d|-)+):((\w|\d|-)+)"
+regex="((\w|\d|-)+):((\w|\d|-)+):((\w|\d|-))"
 
 for keyval in $repos
 do
@@ -13,12 +13,13 @@ do
   then 
     repo="${BASH_REMATCH[1]}"
     upstream="${BASH_REMATCH[3]}"
+    pushToOrigin="${BASH_REMATCH[5]}"
 
     cd $repo
     echo "================================="
     echo "Start syncing $repo"
     echo "================================="
-    syncRepos.sh $upstream
+    syncRepos.sh $upstream $pushToOrigin
     echo "================================="
     echo "Finish syncing $repo"
     echo "================================="
--- syncRepos.sh
+++ syncRepos.sh
@@ -5,9 +5,17 @@ originalBranch=`getCurrentBranch.sh`
 
 if [ -z "$1" ]
 then
-   branch="master"
+   echo "Need to specify a branch"
+   exit
 else
    branch=$1
+fi
+
+if [ -z "$2" ]
+then
+   pushToOrigin=1
+else
+   pushToOrigin=$2
 fi
 
 # Synch the master branch
@@ -15,7 +23,12 @@ git checkout $branch
 git pull --rebase upstream $branch
 
 # Push branches to origin
-git push origin $branch
+if [ "$2" -eq "1" ]
+then
+   git push origin $branch
+else
+   echo "### Not pushing to origin"
+fi
 
 # Switch back to the original branch
 git checkout $originalBranch
